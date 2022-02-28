196459b9bebac1d3833e59b7f1fee1c7a9847c9d
SeaDragon1312 <98462569+SeaDragon1312@users.noreply.github.com>
2022-02-28 16:36:40 +0700

    Delete W1_B6.cpp

--- W1_B6.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    string s1,s2,s3;
-    cin>>s1>>s2>>s3;
-    cout << "Hi " << s3 << ", " <<  s2 << " and " <<  s1 << ".";
-    return 0;
-}
